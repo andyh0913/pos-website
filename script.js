6e66cbe5e6bfe21bc50cdac0215b41dc5fe26ab9
andyh0913 <andyh0913@gmail.com>
2022-03-15 02:10:18 +0800

    Add formal data

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const printButton = document.getElementById("print-button");
 printButton.addEventListener('click', () => {
-    const valueA = document.querySelector('input[name="a"]:checked').value;
-    const valueB = document.querySelector('input[name="b"]:checked').value;
-    const printText = document.getElementById("print-text");
-    printText.textContent = parseInt(valueA) * parseInt(valueB);
+    const domA = document.querySelector('input[name="a"]:checked');
+    const domB = document.querySelector('input[name="b"]:checked');
+    const valueA = domA.value;
+    const valueB = domB.value;
+    const optionA = domA.nextElementSibling.innerHTML;
+    const optionB = domB.nextElementSibling.innerHTML;
+    const printText1 = document.getElementById("print-text-1");
+    const printText2 = document.getElementById("print-text-2");
+    const printText3 = document.getElementById("print-text-3");
+    const result = Math.ceil( (parseFloat(valueA) * parseFloat(valueB) / 10) ).toString();
+
+    printText1.innerHTML = optionA;
+    printText2.innerHTML = optionB;
+    printText3.innerHTML = `${result[0]}.${result[1]}%`;
     window.print();
 })
